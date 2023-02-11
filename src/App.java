d6cbb69cbdfc03c7ea2bbdc1cbe0cf008e301795
Ahmed <Ahmed.ajwad18@gmail.com>
2023-02-11 23:28:01 +0300

    insertion sort

--- src/App.java
+++ src/App.java
@@ -1,41 +1,26 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int[] array = { 5, 9, 3, 7, 3, 7 };
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+        System.out.println("Hello, World!");
+    }
+
+    public static void sortInsertion(int[] sort_arr) {
+
+        for (int i = 0; i < sort_arr.length; ++i) {
+
+            int j = i;
+
+            while (j > 0 && sort_arr[j - 1] > sort_arr[j]) {
+
+                int key = sort_arr[j];
+                sort_arr[j] = sort_arr[j - 1];
+                sort_arr[j - 1] = key;
+                j = j - 1;
+
+            }
         }
     }
 
-    static void sel_sort(int numArray[]) {
-        int n = numArray.length;
-
-        for (int i = 0; i < n - 1; i++) {
-            int min_idx = i;
-            for (int j = i + 1; j < n; j++)
-                if (numArray[j] < numArray[min_idx])
-                    min_idx = j;
-
-            int temp = numArray[min_idx];
-            numArray[min_idx] = numArray[i];
-            numArray[i] = temp;
-        }
-    }
-
-    static void sel_sort(int numArray[]) {
-        int n = numArray.length;
-
-        // traverse unsorted array
-        for (int i = 0; i < n - 1; i++) {
-            // Find the minimum element in unsorted array
-            int min_idx = i;
-            for (int j = i + 1; j < n; j++)
-                if (numArray[j] < numArray[min_idx])
-                    min_idx = j;
-
-            // swap minimum element with compared element
-            int temp = numArray[min_idx];
-            numArray[min_idx] = numArray[i];
-            numArray[i] = temp;
-        }
+    public static void sort(int[] array) {
+        sortInsertion(array);
     }
 }
